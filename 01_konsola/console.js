a00652f0130d5ec93f110ffcf990a3af9b61fdb7
jakubs832 <jakubs832@gmail.com>
2024-09-04 14:38:31 +0200

    sdc

---
+++ 01_konsola/console.js
@@ -0,0 +1,9 @@
+console.log("abc", 404);
+console.log(window);
+console.table([1, 2, 3, "jeden", "dwa", "trzy"]);
+
+console.group("bulbulator");
+console.log("jeden");
+console.log("dwa");
+console.log("trzy");
+console.groupEnd();
